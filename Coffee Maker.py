1906d87b24bca7e493fb7ab082777c35a671be07
Storm <optistorm@gmail.com>
2020-04-27 00:27:32 +0200

    Update to Coffee Maker.py

--- Coffee Maker.py
+++ Coffee Maker.py
@@ -1,29 +1,82 @@
-user_input = int(input("Write how many cups of coffee you will need: > "))
+while True:
+    # Requirements for coffee:
+    coffee_recipe = [200, 50, 15]
 
-# Variables for cup of coffee
-water = 200
-milk = 50
-coffee_beans = 15
+    # User inputs
+    stored_water = int(input("How many ml of water does the coffee machine have? : "))
+    stored_milk = int(input("How many ml of milk does the coffee machine have? : "))
+    stored_coffee_beans = int(input("How many grams of coffee beans does the coffee machine have? : "))
+    required_coffee = int(input("How many cups of coffee do you need? : "))
 
-# Logic:
-if user_input:
-    # Int to string conversion:
-    user_input_conversion = str(user_input)
-    required_water = str(water * user_input)
-    required_milk = str(milk * user_input)
-    required_coffee_beans = str(coffee_beans * user_input)
+    # Calculations:
+    cups_of_water = (stored_water // coffee_recipe[0])
+    cups_of_milk = (stored_milk // coffee_recipe[1])
+    grams_of_beans = (stored_coffee_beans // coffee_recipe[2])
 
-    print("For " + user_input_conversion + " cups of coffee you will need:")
-    print(required_water + "ml of water")
-    print(required_milk + "ml of milk")
-    print(required_coffee_beans + "g of coffee beans")
+    stored_coffee = min(cups_of_water, cups_of_milk, grams_of_beans)
+    excess_coffee = stored_coffee - required_coffee
 
-print('''
-Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!
-''')
+    if required_coffee == stored_coffee:
+        if input("Please type Yes to being preparation: ") == "Yes":
+            print("Yes, I can make that amount of coffee.")
+            print("_________________________________________________")
+            print("Starting to make the coffee... ")
+            print("Grinding coffee beans...")
+            print("Boiling water...")
+            print("Mixing boiled water with crushed coffee beans...")
+            print("Pouring coffee into the cup...")
+            print("Pouring some milk into the cup...")
+            print("Coffee is ready.")
+            print("_________________________________________________")
+            break
+
+        else:
+            print("Turning off")
+            break
+
+    elif required_coffee > stored_coffee:
+        if stored_coffee == 0:
+            print("Please fill up all ingredients please.")
+            break
+
+        if stored_coffee >= 1:
+            print("No, I can make only", stored_coffee, " cups of coffee.")
+            print("Must I still proceed? ")
+
+            if input("Please type Yes to being preparation: ") == "Yes":
+                print("Yes, I can make that amount of coffee.")
+                print("_________________________________________________")
+                print("Starting to make the coffee... ")
+                print("Grinding coffee beans...")
+                print("Boiling water...")
+                print("Mixing boiled water with crushed coffee beans...")
+                print("Pouring coffee into the cup...")
+                print("Pouring some milk into the cup...")
+                print("Coffee is ready.")
+                print("_________________________________________________")
+                break
+
+            else:
+                print("Please restart the process.")
+                break
+        else:
+            print("Please restart the process.")
+            break
+
+    else:
+        if input("Please type Yes to being preparation: ") == "Yes" or input() == "yes":
+            print("Yes, I can make that amount of coffee (and even", excess_coffee, "more than that.)")
+            print("_________________________________________________")
+            print("Starting to make the coffee... ")
+            print("Grinding coffee beans...")
+            print("Boiling water...")
+            print("Mixing boiled water with crushed coffee beans...")
+            print("Pouring coffee into the cup...")
+            print("Pouring some milk into the cup...")
+            print("Coffee is ready.")
+            print("_________________________________________________")
+            break
+
+        else:
+            print("Please restart the process.")
+            break
